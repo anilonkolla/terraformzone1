ba4e36ab19c1fda2dd5691d89a830c353f642fcd
anil <anilreddy@gmail.com>
2023-08-17 12:52:43 +0530

    added dev.tfvars

--- Aug14/provider.tf
+++ Aug14/provider.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "reddy123"
+    key            = "terraform/aws"
+    region         = "ap-south-1"
+    dynamodb_table = "mytable"
+  }
+
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
